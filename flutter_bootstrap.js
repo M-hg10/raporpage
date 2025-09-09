fbcd5c2dc375ad35a6c2b479cc6e5dacde3fecd2
halil gezer <hg_1993@windowslive.com>
2025-09-10 00:40:15 +0300

    qr ve send gönderim işlemleri yapıldı

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3582485204"
+    serviceWorkerVersion: "3716652650"
   }
 });
